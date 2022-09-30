049308b677042104bc61d0f8e1e4e13cfb19769f
snaxdd <nwpress@yandex.ru>
2022-09-30 14:31:12 +0700

    added LinkForm initial files

--- source/app.tsx
+++ source/app.tsx
@@ -1,32 +1,11 @@
 import React from "react";
 import Header from "./components/Header";
-import LinkView from "./components/LinkView";
 
 const App = () => {
   return (
     <>
       <Header />
-      <main>
-        <LinkView
-          index={99}
-          url="https://google.com"
-          shortUrl="https://google.com"
-          transitions={999999}
-        />
-        <LinkView
-          index={98}
-          url="https://google.com"
-          shortUrl="https://google.com"
-          transitions={999999}
-          isEven
-        />
-        <LinkView
-          index={97}
-          url="https://google.com"
-          shortUrl="https://google.com"
-          transitions={999999}
-        />
-      </main>
+      <main></main>
     </>
   );
 };
